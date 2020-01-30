0b4e84b2cd1362195a78f398e185d323d74bdaf8
Haibu64 <haibu64@gmail.com>
2020-01-30 11:48:49 -0700

    increased enemy attack rate.

--- Assets/Scripts/acolyteAttackDistance.cs
+++ Assets/Scripts/acolyteAttackDistance.cs
@@ -28,7 +28,7 @@ public class acolyteAttackDistance : MonoBehaviour {
 	{
 		if (col2D.gameObject.tag == "PlayerCharacter" && AB.invincibilityCooldownCurrent <= 0 && AB.canAttack && !PIS.isDead) {
 			AB.isWithinAttackDistance = true;
-			attackRNG = Random.Range (1, 200);
+			attackRNG = Random.Range (1, 120);
 
 			if (attackRNG == 5 && AB.canAttack) {
 				AB.canAttack = false;
