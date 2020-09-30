20d1bbd64af8a9527f42abe232b2c4f74070c50c
Joshua Young <joshua.r.young1@gmail.com>
2020-09-30 16:29:14 -0600

    adds footsies rough draft

--- Assets/Scripts/acolyteAttackDistance.cs
+++ Assets/Scripts/acolyteAttackDistance.cs
@@ -12,52 +12,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class acolyteAttackDistance : MonoBehaviour {
-	public acolyteBehavior AB;
-	public PlayerInputScript PIS;
-	public Animator anim;
-	public AudioSource audioSrc;
-	public AudioClip punchSoundEffect;
-	public int attackRNG;
-	// Use this for initialization
-	void Start () {
-		audioSrc = GetComponentInParent<AudioSource>();
-	}
+public class acolyteAttackDistance : MonoBehaviour
+{
+  public GameObject playerGameObject;
+  public GameObject enemyGameObject;
+  public Collider2D collider2D;
+  public acolyteBehavior AB;
+  public PlayerInputScript PIS;
+  public Animator anim;
+  public bool isColidingWithSomething;
+  // Use this for initialization
+  void Start()
+  {
+    playerGameObject = GameObject.FindWithTag("PlayerCharacter");
+    enemyGameObject = gameObject.transform.parent.gameObject;
+  }
 
-	void OnTriggerStay2D(Collider2D col2D)
-	{
-		if (col2D.gameObject.tag == "PlayerCharacter" && AB.invincibilityCooldownCurrent <= 0 && AB.canAttack && !PIS.isDead) {
-			AB.isWithinAttackDistance = true;
-			attackRNG = Random.Range (1, 120);
+  void OnTriggerEnter2D(Collider2D col2D)
+  {
+    isColidingWithSomething = true;
+    collider2D = col2D;
+    if (col2D.gameObject.tag == "PlayerCharacter" && AB.invincibilityCooldownCurrent <= 0 && AB.canAttack && !PIS.isDead)
+    {
+      Debug.Log("Player Collision!");
+      AB.isInFootsiesRange = true;
+    }
+  }
 
-			if (attackRNG == 5 && AB.canAttack) {
-				AB.canAttack = false;
-				anim.SetBool ("isLightPunching", true);
-				audioSrc.clip = punchSoundEffect;
-				audioSrc.enabled = true;
-				audioSrc.Play();
-			} else if(attackRNG == 1 && AB.canAttack) {
-				AB.canAttack = false;
-				anim.SetBool ("isHeavyPunching", true);
-				audioSrc.clip = punchSoundEffect;
-				audioSrc.enabled = true;
-				audioSrc.Play();
-			}
-			
-			else {
-				if(AB.canAttack) {
-					anim.SetBool ("isLightPunching", false);
-					anim.SetBool ("isHeavyPunching", false);
-				}
-			}
-		}
-	}
-
-	// void OnTriggerExit2D(Collider2D col2D)
-	// {
-	// 	if (col2D.gameObject.tag == "PlayerCharacter") {
-	// 		AB.isWithinAttackDistance = false;
-	// 		anim.SetBool ("isPunching", false);
-	// 	}
-	// }
+  void OnTriggerExit2D(Collider2D col2D)
+  {
+    isColidingWithSomething = false;
+    collider2D = null;
+    if (col2D.gameObject.tag == "PlayerCharacter")
+    {
+      AB.isInFootsiesRange = false;
+      // anim.SetBool ("isPunching", false);
+    }
+  }
 }
